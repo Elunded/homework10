0de42787766382d46c862394444a847bdf9e64b9
Elunded <demonschenel@gmail.com>
2019-11-07 14:08:30 +0200

    Classwork1

--- array/src/package1/Main.java
+++ array/src/package1/Main.java
@@ -2,19 +2,13 @@ package package1;
 
 public class Main {
     public static void main(String[] args) {
-        int[] x = {-7, 5, -1, 25, 23};
+        int[] x = {-7, 5, -1, 26, 23};
         int min = (int) Double.POSITIVE_INFINITY;
         int max = (int) Double.NEGATIVE_INFINITY;
         int sum = 0, sum2 = 0, ser = 0, bi = 0;
-        for (int i = 0, n = x.length, sum3 = 0; i<n; i++){
-            sum3 += x[i];
-            ser = sum3/n;
-            if (x[i]>ser){
-                bi++;
-            }
-        }
+
         for (int i = 0, n = x.length; i < n; i++) {
-            if (x[i] > 0) {
+            if (x[i]%2 == 0) {
                 sum += x[i];
                 if (x[i] > 20) {
                     if (x[i] < 30) {
@@ -22,16 +16,21 @@ public class Main {
                     }
                 }
             }
-            if (x[i] > max) {
-                max = x[i];
-            }
-            if (x[i]<0){
-                x[i] = Math.abs(x[i]);
-            }
 
         }
-        System.out.println(sum + " " + sum2 + " " + max + " "+ (bi-1));
-        ///kjyewrkjykjtry
+        for (int i = 0, n = x.length; i < n; i++) {
+            if (x[i]>0){
+                x[i]++;
+            } else {
+                x[i]--;
+            }
+        }
+        System.out.println(sum + " " + sum2 + " - Сума від 20 до 30");
+        System.out.println();
+        for (int i:x
+             ) {
+            System.out.print(i + " ");
+        }
     }
 }
 
